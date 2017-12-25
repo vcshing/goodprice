745bc3e412c69d68b6c2291e223dd768fb9287c7
vcshing <vcshing2005@hotmail.com>
2017-12-26 01:38:20 +0800

    asd

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -7,14 +7,20 @@ module.exports = [
         "clobbers": [
             "window.StatusBar"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-console": "1.0.7",
-    "cordova-plugin-statusbar": "1.0.1"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-console": "1.1.0",
+    "cordova-plugin-statusbar": "2.4.0"
 }
 // BOTTOM OF METADATA
 });
