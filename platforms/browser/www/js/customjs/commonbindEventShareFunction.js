2963d93fc3217641ff7e01fc3e03b007facbe5f3
vcshing <vcshing2005@hotmail.com>
2017-12-26 01:00:56 +0800

    asd

---
+++ platforms/browser/www/js/customjs/commonbindEventShareFunction.js
@@ -0,0 +1,37 @@
+$(".otherApp").bind("click", function(e) {
+    e.preventDefault();
+    var target = "_system";
+    var options = "location=yes";
+    var url = "https://play.google.com/store/apps/developer?id=Sky+Explorer";
+    window.open(url, target, options);
+})
+
+$(".shareApp").bind("click", function(e) {
+    window.plugins.socialsharing.share(appConfigArr["appName"], appConfigArr["appDescription"], "", appConfigArr["androidAppLink"]);
+})
+
+$(".langen").bind("click", function() {
+    lang = "en";
+    storageManager.setCookie("lang", {
+        "selectedLang": lang
+    });
+    pageInit();
+
+})
+
+$(".langtc").bind("click", function() {
+    //  alert(1);
+    lang = "zh-tw";
+    storageManager.setCookie("lang", {
+        "selectedLang": lang
+    });
+    pageInit();
+})
+
+$(".langDefault").bind("click", function() {
+    lang = checkLang();
+    storageManager.setCookie("lang", {
+        "selectedLang": lang
+    });
+    pageInit();
+})
